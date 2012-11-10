003087c5de5be16e2af7a5ec66ee716c6df4506d
Dan Tasse <dan.tasse@gmail.com>
2012-11-10 18:29:00 -0500

    got date, from, to, cc... next just the message text

--- process_mail.py
+++ process_mail.py
@@ -6,20 +6,33 @@
 # Some details from:
 # https://yuji.wordpress.com/2011/06/22/python-imaplib-imap-example-with-gmail/
 
-import email, os
+import datetime, email, os
+
+def remove_trn(s):
+    return s.replace('\n', ' ').replace('\r', ' ').replace('\t', ' ')
 
 path = '/Users/dtasse/Desktop/emails/'
 
 for filename in os.listdir(path):
-  contents = open(path + filename, 'r').read()
-  msg = email.message_from_string(contents)
-  
-  from_address = email.utils.parseaddr(msg['From'])[1]
+    contents = open(path + filename, 'r').read()
+    msg = email.message_from_string(contents)
+    
+    from_str = remove_trn(msg['From'])
+    from_addr = email.utils.parseaddr(from_str)[1]
 
-  # getaddresses returns a list of ("Dan Tasse", "dantasse@cmu.edu") tuples
-  tos = msg.get_all('to', [])
-  to_addrs = [addr[1] for addr in email.utils.getaddresses(tos)]
+    # getaddresses returns a list of ("Dan Tasse", "dantasse@cmu.edu") tuples
+    tos = msg.get_all('to', [])
+    to_addrs = [remove_trn(addr[1]).lower() for addr in email.utils.getaddresses(tos)]
 
-  ccs = msg.get_all('cc', [])
-  cc_addrs = [addr[1] for addr in email.utils.getaddresses(ccs)]
+    ccs = msg.get_all('cc', [])
+    cc_addrs = [remove_trn(addr[1]).lower() for addr in email.utils.getaddresses(ccs)]
 
+    date_tuple = email.utils.parsedate_tz(msg['date'])
+    if date_tuple:
+        utc_date = datetime.datetime.fromtimestamp(email.utils.mktime_tz(date_tuple))
+    else:
+        print "error parsing date: " + msg['date']
+
+    print "%s\t%s\t%s\t%s" % (utc_date, from_addr, to_addrs, cc_addrs)
+
+    # TODO get the actual message text
