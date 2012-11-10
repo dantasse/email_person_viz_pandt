728c684f9f302663ad4d8247ae37b429d825b6a4
Dan Tasse <dan.tasse@gmail.com>
2012-11-10 16:51:07 -0500

    forget oauth, just use username-password

---
+++ get_mail.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+#
+# Script to get all your email from gmail.
+#
+# From this Stack Overflow post:
+# http://stackoverflow.com/questions/348630/how-can-i-download-all-emails-with-attachments-from-gmail
+
+import email, getpass, imaplib, os
+
+detach_dir = '.' # directory where to save attachments (default: current)
+print "This application will attempt to save all your gmail to files. \
+    If this is not what you want, quit now with ctrl-c." 
+user = raw_input("Enter your GMail username:")
+pwd = getpass.getpass("Enter your password: ")
+
+# connecting to the gmail imap server
+m = imaplib.IMAP4_SSL("imap.gmail.com")
+m.login(user,pwd)
+m.select("[Gmail]/All Mail") # here you a can choose a mail box like INBOX instead
+# use m.list() to get all the mailboxes
+
+resp, items = m.search(None, "ALL") # you could filter using the IMAP rules here (check http://www.example-code.com/csharp/imap-search-critera.asp)
+items = items[0].split() # getting the mails id
+print "Got this many items"
+print len(items)
+
+# let's save them each to a file so we don't have to keep asking gmail
+email_id = 0
+for emailid in items:
+    resp, data = m.fetch(emailid, "(RFC822)") # fetching the mail, "`(RFC822)`" means "get the whole stuff", but you can ask for headers only, etc
+    email_body = data[0][1]
+    output_file = open('/Users/dtasse/Desktop/emails/email_' + str(email_id) + '.txt', 'w')
+    email_id += 1
+    output_file.write(email_body)
+    output_file.close()
+    # email_message = email.message_from_string(email_body) # parsing the mail content to get a mail object
+    # print email_message['To']
+"""
+    #Check if any attachments at all
+    if mail.get_content_maintype() != 'multipart':
+        continue
+
+    print "["+mail["From"]+"] :" + mail["Subject"]
+
+    # we use walk to create a generator so we can iterate on the parts and forget about the recursive headach
+    for part in mail.walk():
+        # multipart are just containers, so we skip them
+        if part.get_content_maintype() == 'multipart':
+            continue
+
+        # is this part an attachment ?
+        if part.get('Content-Disposition') is None:
+            continue
+
+        filename = part.get_filename()
+        counter = 1
+
+        # if there is no filename, we create one with a counter to avoid duplicates
+        if not filename:
+            filename = 'part-%03d%s' % (counter, 'bin')
+            counter += 1
+
+        att_path = os.path.join(detach_dir, filename)
+
+        #Check if its already there
+        if not os.path.isfile(att_path) :
+            # finally write the stuff
+            fp = open(att_path, 'wb')
+            fp.write(part.get_payload(decode=True))
+            fp.close()
+"""
