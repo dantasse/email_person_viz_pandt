83012a04f0c6b3d388abb284bf53ceb82034379a
Dan Tasse <dan.tasse@gmail.com>
2012-11-18 21:10:23 -0500

    now you can search by date

--- generate_snippets.py
+++ generate_snippets.py
@@ -11,8 +11,7 @@ cc addresses
 rest of the text
 """
 
-import os, random
-import argparse
+import argparse, datetime, os, random
 
 parser = argparse.ArgumentParser(description='Display some "meaningful"\
     snippets, given email between you and another person.')
@@ -22,9 +21,11 @@ parser.add_argument('me',
 parser.add_argument('person',
     help='the email address of the person you want to get snippets from')
 parser.add_argument('--start_date',
-    help='the start date of the range you want to search, in format YYYYMMDD')
+    default='2012-10-01',
+    help='the start date of the range you want to search, in format YYYY-MM-DD')
 parser.add_argument('--end_date',
-    help='the end date of the range you want to search, in format YYYYMMDD')
+    default='2012-12-31',
+    help='the end date of the range you want to search, in format YYYY-MM-DD')
 parser.add_argument('-p', '--emails_path',
     default='/Users/dtasse/Desktop/processed_emails/',
     help='the path to the directory with all the emails')
@@ -32,6 +33,12 @@ parser.add_argument('-n', '--num_snippets', type=int, default=1,
     help='the number of snippets to get')
 
 args = parser.parse_args()
+start_date = datetime.datetime.strptime(args.start_date, '%Y-%m-%d')
+end_date = datetime.datetime.strptime(args.end_date, '%Y-%m-%d')\
+    + datetime.timedelta(days=1)
+# Adding a day to facilitate comparisons. This is creating a date at 00:00:00,
+# so if end date is May 3, let's compare to the first second of May 4, not the
+# first second of May 3.
 
 # TODO: this is not yet perfect. Some emails, particularly long ones, have some
 # of the reply lines not starting with >'s, due to long line wrapping or
@@ -70,13 +77,18 @@ def read_email(filepath):
     from_address = contents[1].strip()
     # note that we expect to_addresses to be safe
     to_addresses = eval(contents[2].strip())
+    date_string = contents[0].strip()
+    #TODO: I never specified that the string should be in this format when
+    # it is written to the file in the first place. I should do that.
+    date = datetime.datetime.strptime(date_string, '%Y-%m-%d %H:%M:%S')
     if ((args.person == from_address and args.me in to_addresses) or\
-        (args.person in to_addresses and args.me == from_address)):
-        # TODO AND date >= start_date AND date <= end_date
-        print filepath
+        (args.person in to_addresses and args.me == from_address)) and\
+        date >= start_date and date <= end_date:
+        # TODO Off by ~5 hrs due to timezones. Don't care for now, but would
+        # be nice to fix someday.
         msg_lines = contents[4:]
         msg_lines_noreplies = remove_replies(msg_lines)
-        words = ''.join(msg_lines_noreplies).split()
+        msg_text = ''.join(msg_lines_noreplies)
  
 for filename in os.listdir(args.emails_path):
     read_email(args.emails_path + filename)
