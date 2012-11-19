0a2d82734a54bf49df860a8a322d50f2c9d8e9d6
Dan Tasse <dan.tasse@gmail.com>
2012-11-18 21:42:34 -0500

    a little refactoring, now reads in each mail and tells whether it is good

--- generate_snippets.py
+++ generate_snippets.py
@@ -21,11 +21,13 @@ parser.add_argument('me',
 parser.add_argument('person',
     help='the email address of the person you want to get snippets from')
 parser.add_argument('--start_date',
-    default='2012-10-01',
-    help='the start date of the range you want to search, in format YYYY-MM-DD')
+    default='0001-01-01',
+    help='the start date (inclusive) of the range you want to search, in\
+          format YYYY-MM-DD')
 parser.add_argument('--end_date',
-    default='2012-12-31',
-    help='the end date of the range you want to search, in format YYYY-MM-DD')
+    default='9999-12-30',
+    help='the end date (inclusive) of the range you want to search, in format\
+          YYYY-MM-DD')
 parser.add_argument('-p', '--emails_path',
     default='/Users/dtasse/Desktop/processed_emails/',
     help='the path to the directory with all the emails')
@@ -71,25 +73,45 @@ def remove_replies(msg_lines):
             good_lines.append(line)
     return good_lines
 
+class email:
+    """ represents an email """
+    def __init__(self, date, from_address, to_addresses, cc_addresses, text):
+        self.date = date
+        self.from_address = from_address
+        self.to_addresses = to_addresses
+        self.cc_addresses = cc_addresses
+        self.text = text
+
 def read_email(filepath):
     file = open(filepath, 'r')
     contents = file.readlines()
-    from_address = contents[1].strip()
-    # note that we expect to_addresses to be safe
-    to_addresses = eval(contents[2].strip())
     date_string = contents[0].strip()
     #TODO: I never specified that the string should be in this format when
     # it is written to the file in the first place. I should do that.
     date = datetime.datetime.strptime(date_string, '%Y-%m-%d %H:%M:%S')
-    if ((args.person == from_address and args.me in to_addresses) or\
-        (args.person in to_addresses and args.me == from_address)) and\
-        date >= start_date and date <= end_date:
-        # TODO Off by ~5 hrs due to timezones. Don't care for now, but would
-        # be nice to fix someday.
-        msg_lines = contents[4:]
-        msg_lines_noreplies = remove_replies(msg_lines)
-        msg_text = ''.join(msg_lines_noreplies)
+    from_address = contents[1].strip()
+    # note that we expect to_addresses to be safe
+    to_addresses = eval(contents[2].strip())
+    cc_addresses = eval(contents[3].strip())
+    msg_lines = contents[4:]
+    msg_lines_noreplies = remove_replies(msg_lines)
+    msg_text = ''.join(msg_lines_noreplies)
+
+    return email(date, from_address, to_addresses, cc_addresses, msg_text)
  
+def is_email_good(email):
+    is_right_people =\
+        (args.person == email.from_address and args.me in email.to_addresses) or\
+        (args.person in email.to_addresses and args.me == email.from_address)
+    # TODO Off by ~5 hrs due to timezones. Don't care for now, but would
+    # be nice to fix someday.
+    is_date_okay = email.date >= start_date and email.date <= end_date
+    return is_right_people and is_date_okay
+
+count = 0
 for filename in os.listdir(args.emails_path):
-    read_email(args.emails_path + filename)
+    e1 = read_email(args.emails_path + filename)
+    if is_email_good(e1):
+        count += 1
+print count
 
