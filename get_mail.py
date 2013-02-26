78ac7970dd02a9794489892743b9201e6440896b
Dan Tasse <dan.tasse@gmail.com>
2013-02-26 01:05:13 -0500

    a little bit of crazy pants hacky parallelism

--- get_mail.py
+++ get_mail.py
@@ -11,6 +11,7 @@
 # https://yuji.wordpress.com/2011/06/22/python-imaplib-imap-example-with-gmail/
 
 import argparse, email, getpass, imaplib, os, time
+from multiprocessing import Process
 
 parser = argparse.ArgumentParser(description='Downloads all your email.')
 parser.add_argument('path', help='Directory to save all your emails to.\
@@ -33,21 +34,39 @@ resp, items = m.search(None, "ALL") # you could filter using the IMAP rules here
 items = items[0].split() # getting the mails id
 print "Number of emails to download: " + str(len(items))
 
-# let's save them each to a file so we don't have to keep asking gmail
-email_id = 0
-start_time = time.time()
-for emailid in items:
-    resp, data = m.fetch(emailid, "(RFC822)") # fetching the mail, "`(RFC822)`" means "get the whole stuff", but you can ask for headers only, etc
+m2 = imaplib.IMAP4_SSL("imap.gmail.com")
+m2.login(user,pwd)
+m2.select("[Gmail]/All Mail", readonly=True) # do I have to do this again?
+
+
+def get_one_email(mail_conn, id):
+    resp, data = mail_conn.fetch(id, "(RFC822)") # fetching the mail, "`(RFC822)`" means "get the whole stuff", but you can ask for headers only, etc
     email_body = data[0][1]
-    output_file = open(args.path + 'email_' + str(email_id) + '.txt', 'w')
-    email_id += 1
+    output_file = open(args.path + 'email_' + str(id) + '.txt', 'w')
     output_file.write(email_body)
     output_file.close()
-    if (email_id % 100 == 0):
-        print 'This many messages downloaded: ' + str(email_id)
-        num_sec = time.time() - start_time
-        print 'This many seconds: ' + str(num_sec)
-        rate = email_id / num_sec
-        print 'Rate: ' + str(rate) + ' messages per second'
-        est_sec_left = (len(items) - email_id) / rate
-        print 'Estimated minutes left: ' + str(est_sec_left / 60)
+
+def get_some_emails(mail_conn, ids):
+    for id in ids:
+        get_one_email(mail_conn, id)
+
+p2 = Process(target=get_some_emails, args=(m2, items[30000:]))
+p2.start()
+
+# TODO: split up evenly among processes, show some timing displays
+get_some_emails(m, items[0:30000])
+
+
+# start_time = time.time()
+# emails_processed = 0
+# for i in items[30000:40000]:
+#     get_one_email(i)
+#     emails_processed += 1
+#     if (emails_processed % 100 == 0):
+#         print 'This many messages downloaded: ' + str(emails_processed)
+#         num_sec = time.time() - start_time
+#         print 'This many seconds: ' + str(num_sec)
+#         rate = emails_processed / num_sec
+#         print 'Rate: ' + str(rate) + ' messages per second'
+#         est_sec_left = (len(items) - emails_processed) / rate
+#         print 'Estimated minutes left: ' + str(est_sec_left / 60)
