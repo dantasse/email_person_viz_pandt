851b0978ab07712a471239f29a5d86fba0100ba2
Dan Tasse <dan.tasse@gmail.com>
2013-02-26 00:07:07 -0500

    timing experiments 1

--- get_mail.py
+++ get_mail.py
@@ -10,7 +10,7 @@
 # and
 # https://yuji.wordpress.com/2011/06/22/python-imaplib-imap-example-with-gmail/
 
-import argparse, email, getpass, imaplib, os
+import argparse, email, getpass, imaplib, os, time
 
 parser = argparse.ArgumentParser(description='Downloads all your email.')
 parser.add_argument('path', help='Directory to save all your emails to.\
@@ -35,13 +35,27 @@ print "Number of emails to download: " + str(len(items))
 
 # let's save them each to a file so we don't have to keep asking gmail
 email_id = 0
+start_time = time.time()
+time_spent_network = 0
+time_spent_fs = 0
 for emailid in items:
+    net_time_start = time.time()
     resp, data = m.fetch(emailid, "(RFC822)") # fetching the mail, "`(RFC822)`" means "get the whole stuff", but you can ask for headers only, etc
+    time_spent_network += (time.time() - net_time_start)
+    fs_time_start = time.time()
     email_body = data[0][1]
     output_file = open(args.path + 'email_' + str(email_id) + '.txt', 'w')
     email_id += 1
     output_file.write(email_body)
     output_file.close()
+    time_spent_fs += (time.time() - fs_time_start)
     if (email_id % 100 == 0):
         print 'This many messages downloaded: ' + str(email_id)
-
+        num_sec = time.time() - start_time
+        print 'This many seconds: ' + str(num_sec)
+        rate = email_id / num_sec
+        print 'Rate: ' + str(rate) + ' messages per second'
+        est_sec_left = (len(items) - email_id) / rate
+        print 'Estimated minutes left: ' + str(est_sec_left / 60)
+        print 'Time spent over network: ' + str(time_spent_network)
+        print 'Time spent in file system: ' + str(time_spent_fs)
