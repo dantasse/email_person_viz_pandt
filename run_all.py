ef7f1b100df6f8d0944dbf620486486d61a00664
Dan Tasse <dan.tasse@gmail.com>
2013-03-24 13:30:21 -0400

    a couple more warning lines

--- run_all.py
+++ run_all.py
@@ -7,6 +7,10 @@ from subprocess import call
 
 print "This application will attempt to save all your gmail to files.\
  If this is not what you want, quit now with ctrl-c." 
+print "Make sure your computer is set to not sleep. (on a mac, set this in\
+ System Preferences -> Energy Saver)"
+print "Also, make sure there's no folder called emails/ already; if there is,\
+ it'll get clobbered."
 email_address = raw_input("Enter your email address: ")
 retval = call(['./get_mail.py', '--email_address=' + email_address])
 if retval == 0:
